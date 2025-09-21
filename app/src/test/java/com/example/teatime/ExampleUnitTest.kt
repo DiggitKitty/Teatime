e9678d41ef594d56465141c40a6d1fe5fee2d7f7
Markus <Mar.Jae@web.de>
2025-09-21 14:42:41 +0200

    Fix unit & instrumented tests by adding dependencies to junit & androidx testing

--- app/src/test/java/com/example/teatime/ExampleUnitTest.kt
+++ app/src/test/java/com/example/teatime/ExampleUnitTest.kt
@@ -1,17 +1,12 @@
 package com.example.teatime
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
+    fun `Adding two numbers returns the expected result`() {
         assertEquals(4, 2 + 2)
     }
 }
